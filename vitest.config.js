4f41ddb74982b30539a0924ceacefc06904da1b7
Cristian Etchebarne <cristianmetchebarne@gmail.com>
2022-11-18 18:56:50 -0300

    Updated coverage requirements

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,14 @@ export default defineConfig({
   test: {
     setupFiles: ["./vitest-setup.js"],
     environment: "jsdom",
+    coverage: {
+      all: true,
+      include: ["src"],
+      exclude: ["src/index.js"],
+      lines: 50,
+      statements: 50,
+      functions: 50,
+      branches: 50,
+    },
   },
 });
